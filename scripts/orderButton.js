d2103b60b03cadfc26dc3493758d7f86ca2c87e2
James Freeman <jamesfreeman114@gmail.com>
2025-10-02 16:15:12 -0500

    Added customized orders

--- scripts/orderButton.js
+++ scripts/orderButton.js
@@ -1,4 +1,11 @@
-import { placeOrder } from "./transientState.js"
+import { placeOrder, setCustomId } from "./transientState.js"
+
+const handleCustomChoice = (event) => {
+    if (event.target.name === "custom-type") {
+       setCustomId(parseInt(event.target.value))
+    }
+}
+
 
 const handleOrderSubmission = (clickEvent) => {
     if (clickEvent.target.id === "order-button") {
@@ -8,9 +15,29 @@ const handleOrderSubmission = (clickEvent) => {
 
 }
 
-export const orderSubmissionButton = () => {
+export const orderSubmissionButton = async () => {
+    document.addEventListener("change", handleCustomChoice)
     document.addEventListener("click", handleOrderSubmission)
 
-    return `<button id="order-button">Place Order</button>`
+    const response = await fetch("http://localhost:8088/customs")
+    const customOptions = await response.json()
+
+    let html =`<div class="place-orders">
+               <div class="custom-orders">`
+
+    const customOptionHTML = customOptions.map(
+        (customType) => {
+            return `<input type="radio" name="custom-type" value=${customType.id}/>${customType.name}`
+        })
+
+    html += customOptionHTML.join("")
+    
+    html += `</div>
+            <button id="order-button">Place Order</button>
+            </div>
+            `
+
+    return html 
+    
 
 }
