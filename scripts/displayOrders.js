c25de5cecb49c6c62dc3bab1918e7572b13ef03e
James Freeman <jamesfreeman114@gmail.com>
2025-10-02 12:41:37 -0500

    Used expand to calculate and show total prices

--- scripts/displayOrders.js
+++ scripts/displayOrders.js
@@ -1,14 +1,17 @@
 export const Orders = async () => {
-    const fetchResponse = await fetch("http://localhost:8088/orders")
+    const fetchResponse = await fetch("http://localhost:8088/orders?_expand=metal&_expand=style&_expand=size")
     const orders = await fetchResponse.json()
-
+    
     let html = ""
 
     let ordersHTML = orders.map(
         (order) => {
+
+            const orderPrice = order.metal.price + order.style.price + order.size.price 
+
             return `
                 <section class="order-submission-container">
-                    <h3>Order #${order.id}</h3>
+                    <h3>Order #${order.id} Cost: ${orderPrice}</h3>
                 </section>
             
             `
@@ -20,5 +23,7 @@ export const Orders = async () => {
          html += ordersHTML.join("")    
 
          return html
+         
+
 
 }
